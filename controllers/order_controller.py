987b8c4c23ef060b45b48317e7298822808c4a91
Арсен Зиядинов <>
2025-03-09 14:41:43 +0300

    database_controller.py optimization

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -3,5 +3,5 @@ from models import Order
 
 
 def calculate_order_price(order: Order, price: int) -> int:
-    dict_colors, dict_details = database_controller.load_base()
+    dict_colors, dict_details = database_controller.get_dict_colors(), database_controller.get_dict_details()
     return price * dict_colors[order.color] + price * dict_details[order.detail]
