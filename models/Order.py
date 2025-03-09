8dfca088d65ed5e67dc11104bb348814922dde15
Арсен Зиядинов <>
2025-03-09 14:34:21 +0300

    added getters for Order.py

--- models/Order.py
+++ models/Order.py
@@ -1,4 +1,12 @@
 class Order:
     def __init__(self, detail: str, color: str):
-        self.detail = detail
-        self.color = color
+        self.__detail = detail
+        self.__color = color
+
+    @property
+    def color(self):
+        return self.__color
+
+    @property
+    def detail(self):
+        return self.__detail
